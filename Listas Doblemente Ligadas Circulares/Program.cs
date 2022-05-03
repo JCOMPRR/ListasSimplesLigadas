589cb7dab2d13ad7746b65e10c3ea8e8766226a2
Jose Carlos Rodriguez <rodjosecarlos15@gmail.com>
2022-05-03 13:19:58 -0700

    Agregue el codigo de program de listas simples circulares

--- Listas Doblemente Ligadas Circulares/Program.cs
+++ Listas Doblemente Ligadas Circulares/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using Listas_Circulares;
+
+Lista listas = new Lista();
+listas.AgregarNodo("Cero");
+listas.AgregarNodo("Uno");
+listas.AgregarNodo("Dos");
+listas.AgregarNodo("Tres");
+Console.WriteLine(listas.RecorrerLista());
+
+
+Console.WriteLine();
+
+
+Console.WriteLine("Buscando Nodo");
+Nodo nodoDos = listas.Buscar("Dos");
+Console.WriteLine(nodoDos.Valor);
+
+Console.WriteLine();
+
+Nodo nodoPosteriorAtres = listas.Buscar("Tres");
+//Console.WriteLine(nodoPosteriorAtres.Enlace.Enlace.Valor);
+
+Console.WriteLine();
+
+
+Console.WriteLine("Se borro nodo");
+listas.BorrarNodo("Dos");
+Console.WriteLine(listas.RecorrerLista());
