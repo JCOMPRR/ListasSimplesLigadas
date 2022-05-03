133fa7bf7439ee3db58ed0e561c6dcb0ffbcc06e
Jose Carlos Rodriguez <rodjosecarlos15@gmail.com>
2022-05-02 18:19:21 -0700

    En Listas agregue El buscar anterior, Borrar nodo y el buscar indice (restante), cree tambien agregue el proyecto Listas circulares junto con sus clases listas y nodos

--- ListasDoblementeLigadas/Lista.cs
+++ ListasDoblementeLigadas/Lista.cs
@@ -69,4 +69,60 @@ namespace ListasDoblementeLigadas
             return null;
         }
 
+        public Nodo BuscarPorIndice(int indice)
+        {
+            int Indice = -1;
+            if (ValidaVacio() == false)
+            {
+                Nodo nodoBusqueda = nodoInicial;
+                while (nodoBusqueda.EnlaceSiguiente != null)
+                {
+                    nodoBusqueda = nodoBusqueda.EnlaceSiguiente;
+                    Indice++;
+                    if (Indice == indice)
+                    {
+                        return nodoBusqueda;
+                    }
+                }
+            }
+            return null;
+
+        }
+
+        public Nodo BuscarAnterior(string dato)
+        {
+            if (ValidaVacio() == false)
+            {
+                Nodo nodoBusqueda = nodoInicial;
+                while (nodoBusqueda.EnlaceSiguiente != null && nodoBusqueda.EnlaceSiguiente.Valor != dato)
+                {
+                    nodoBusqueda = nodoBusqueda.EnlaceSiguiente;
+                    if (nodoBusqueda.EnlaceSiguiente.Valor == dato)
+                    {
+                        return nodoBusqueda;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public void BorrarNodo(string dato)
+        {
+            if (ValidaVacio() == false)
+            {
+                nodoActual = Buscar(dato);
+                if (nodoActual != null)
+                {
+                    Nodo nodoAnterior = BuscarAnterior(dato);
+                    nodoAnterior.EnlaceSiguiente = nodoActual.EnlaceSiguiente;
+                    nodoActual.EnlaceSiguiente = nodoAnterior;
+                }
+            }
+        }
+
+
+
+
+
+    }
 }
